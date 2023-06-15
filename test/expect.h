5904f8a5b394e9b44761378417991f3c796304c2
Artem Kan <kan.art.email@gmail.com>
2023-06-15 11:54:06 +0500

    fix(test): correct EXPECT for release

--- test/expect.h
+++ test/expect.h
@@ -1,14 +1,13 @@
 #include <assert.h>
 
-#ifndef NDEBUG
-#define EXPECT(x) assert(x)
-#else
-#define EXPECT(x)                                                              \
-  do {                                                                         \
-    typeof((x)) r = (x);                                                       \
-    if (!r) {                                                                  \
-      fprintf(stderr, "EXPECT(%s) failed with %d\n", #x, r);                   \
-      exit(EXIT_FAILURE);                                                      \
-    }                                                                          \
+#include "ks/ret.h"
+
+#define EXPECT(x)                                     \
+  do {                                                \
+    typeof((x)) r = (x);                              \
+    if (!r)                                           \
+    {                                                 \
+      ks_error("EXPECT(%s) failed with %d\n", #x, r); \
+      exit(EXIT_FAILURE);                             \
+    }                                                 \
   } while (0)
-#endif 
